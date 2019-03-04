7454549fc60d3e2864e6b7a4da37ae17e2660d7c
Tiago Cordeiro <tiago@mulhergorila.com>
2019-03-04 00:27:42 -0300

    Configura testes básicos.
    This close #3

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import RequestFactory, TestCase, Client
 
-# Create your tests here.
+from .views import index
+
+
+class HomeViewTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.client = Client()
+
+    def test_home_view_status_code_is_ok(self):
+        request = self.factory.get('/')
+
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
