1c6e0aa3b4c6a13234f1735cae97f86cc80fda44
Tiago Cordeiro <tiago@mulhergorila.com>
2020-01-17 16:04:59 -0300

    Update Dependencies.
    This close #149

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,6 +1,6 @@
 from django.test import RequestFactory, TestCase, Client
 
-from .models import Project
+from .models import Project, Category
 from .views import portfolio, portfolio_detail
 
 
@@ -10,9 +10,13 @@ class PortfolioViewTests(TestCase):
         self.factory = RequestFactory()
         self.client = Client()
 
+        # Test Category
+        self.category_sample = Category.objects.create(name='Sample')
+
         # Portfolio projects
         self.portfolio_web_01 = Project.objects.create(title='Projeto Web 01',
-                                                       description='Projeto web Teste')
+                                                       description='Projeto web Teste',
+                                                       category=self.category_sample)
 
     def test_portfolio_view_status_code_is_ok(self):
         request = self.factory.get('/portfolio/')
@@ -26,8 +30,14 @@ class PortfolioViewTests(TestCase):
         response = portfolio_detail(request, slug=self.portfolio_web_01.slug)
         self.assertEqual(response.status_code, 200)
 
-    # def test_portfolio_detail_view_does_not_exist(self):
-    #     request = self.factory.get('/portfolio/projeto/projeto-nao-existe')
-    #
-    #     response = portfolio_detail(request, slug='projeto-nao-existe')
-    #     self.assertEqual(response.status_code, 500)
+    def test_project_title_returns(self):
+        projeto = self.portfolio_web_01
+        self.assertEquals('Projeto Web 01', projeto.title)
+
+    def test_project_str_returns(self):
+        projeto_str = self.portfolio_web_01
+        self.assertEquals('Projeto Web 01', projeto_str.__str__())
+
+    def test_category_name_returns(self):
+        categoria = self.category_sample
+        self.assertEquals('Sample', categoria.name)
